5c60993f12a04684769da9d07b369993823b995b
JenWeber <weberj10@gmail.com>
2016-04-01 16:12:08 -0400

    associate organization id to user at sign up

--- scripts/sign-up.sh
+++ scripts/sign-up.sh
@@ -4,18 +4,19 @@ curl --include --request POST http://localhost:3000/sign-up \
   --header "Content-Type: application/json" \
   --data '{
     "credentials": {
-      "email": "dog@dog.com",
+      "email": "gato@gato.com",
       "password": "password",
-      "password_confirmation": "password"
+      "password_confirmation": "password",
+      "organization_id":"1"
     }
   }'
 
-curl --include --request POST http://localhost:3000/sign-up \
-  --header "Content-Type: application/json" \
-  --data '{
-    "credentials": {
-      "email": "cat@cat.com",
-      "password": "password",
-      "password_confirmation": "password"
-    }
-  }'
+# curl --include --request POST http://localhost:3000/sign-up \
+#   --header "Content-Type: application/json" \
+#   --data '{
+#     "credentials": {
+#       "email": "cat@cat.com",
+#       "password": "password",
+#       "password_confirmation": "password"
+#     }
+#   }'
